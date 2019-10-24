0d5f73d125f315ccaef8ef95fbd343383f4f4bb1
maiple <maiple@gmail.com>
2019-10-23 19:53:39 -0700

    initial commit

---
+++ spec/opengml-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import OpengmlAtomView from '../lib/opengml-atom-view';
+
+describe('OpengmlAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
